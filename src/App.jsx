eb17bfdbf9a93feb68252a4bb34a02585f7b731c
Jonathan Alarcon <joona.palarconss@gmail.com>
2025-06-11 17:02:48 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='flex justify-center bg-gradient-to-l from-sky-500 via-purple-500/90 to-indigo-500 p-4 rounded-lg'>
+        <h1 className="text-3xl font-bold text-black ">Portafolio en contrucción</h1>
+      </div>
+    </>
+  );
+}
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  base: "/portafolio/", // <--- importante para GitHub Pages
+  plugins: [
+    react(),
+    tailwindcss(), // <--- plugin Tailwind para Vite
+  ],
+});
