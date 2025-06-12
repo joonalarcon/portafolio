3638798f89e163813ec2566cd78a4a052318257a
Jonathan Alarcon <joona.palarconss@gmail.com>
2025-06-11 22:46:29 -0400

    primer commit en rama desarrollo

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,50 @@
-import { useEffect, useRef, useState } from "react";
 import "./App.css";
 
+import LogoGithub from "./assets/logos/LogoGithub";
+import LogoReact from "./assets/logos/LogoReact";
+import LogoTailwind from "./assets/logos/LogoTailwind";
+import LogoVite from "./assets/logos/LogoVite";
+
 function App() {
-  const [tilt, setTilt] = useState({ x: 0, y: 0 });
-
-  const handleOrientation = (event) => {
-    const x = event.gamma; // izquierda/derecha
-    const y = event.beta; // adelante/atrás
-    setTilt({ x, y });
-  };
-
-  const requestPermission = async () => {
-    if (
-      typeof DeviceOrientationEvent !== "undefined" &&
-      typeof DeviceOrientationEvent.requestPermission === "function"
-    ) {
-      const response = await DeviceOrientationEvent.requestPermission();
-      if (response === "granted") {
-        window.addEventListener("deviceorientation", handleOrientation);
-      } else {
-        alert("Permiso denegado para el sensor");
-      }
-    } else {
-      window.addEventListener("deviceorientation", handleOrientation);
-    }
-  };
-
-  useEffect(() => {
-    return () => {
-      window.removeEventListener("deviceorientation", handleOrientation);
-    };
-  }, []);
-
-  const transformStyle = {
-    transform: `translate(${tilt.x * 1.5}px, ${tilt.y * 1.5}px) rotateX(${
-      tilt.y / 5
-    }deg) rotateY(${tilt.x / 5}deg)`,
-    transition: "transform 0.1s ease-out",
-  };
+  const animationLogos =
+    "transform transition duration-500 hover:scale-110 w-24 h-24 sm:w-28 sm:h-28 md:w-32 md:h-32 lg:w-36 lg:h-36";
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center gap-6 px-4 bg-gray-900 text-white">
-      <h1 className="text-4xl font-bold text-center">Portafolio Jonathan</h1>
+    <div className="min-h-screen flex flex-col items-center justify-center px-4">
+      <header className="text-center mb-10">
+        <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold">
+          Jonathan Alarcón
+        </h1>
+        <h3 className="text-xl sm:text-2xl font-semibold mt-2">
+          Full Stack Developer 2
+        </h3>
+      </header>
 
       <button
-        onClick={requestPermission}
-        className="bg-green-600 hover:bg-green-700 text-white px-4 py-2 rounded">
-        Activar sensor
+        className="flex items-center justify-center bg-black w-40 h-14 rounded-2xl shadow-md transition mb-6"
+        onClick={() => window.open("https://github.com/joonalarcon", "_blank")}>
+        <LogoGithub className="w-8 h-8" />
       </button>
 
-      <button
-        style={transformStyle}
-        className="bg-blue-600 text-white font-bold py-4 px-8 rounded-xl shadow-lg">
-        Mueve tu celular ✨
-      </button>
+      <div className="flex justify-center text-center mb-10 bg-gradient-to-l from-sky-500 via-purple-500/90 to-indigo-500 p-4 rounded-lg w-full max-w-md">
+        <h2 className="text-xl sm:text-2xl font-bold text-black">
+          Portafolio en construcción
+        </h2>
+      </div>
+
+      <div className="flex justify-center gap-6 flex-wrap">
+
+        <LogoVite
+          className={`${animationLogos} animate-wiggle animate-infinite animate-duration-[4000ms] animate-ease-in-out`}
+        />
+       
+        <LogoTailwind
+          className={`${animationLogos} animate-shake animate-duration-[2000ms] animate-infinite animate-ease-in-out `}
+        />
+        <LogoReact
+          className={`${animationLogos} animate-spin animate-infinite animate-duration-[4000ms] animate-ease-in-out`}
+        />
+      </div>
     </div>
   );
 }
